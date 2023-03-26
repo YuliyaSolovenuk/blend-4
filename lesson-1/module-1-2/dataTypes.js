6b5de6d8d3ce8bc28c4ee4cc329c5cc396fc3889
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2023-03-26 14:36:42 +0300

    add lesson-1

---
+++ lesson-1/module-1-2/dataTypes.js
@@ -0,0 +1,49 @@
+//TODO: типи даних в js
+
+// const str = `hello`;
+// const num = 234;
+// const symb = Symbol('hello');
+// const bool = true;
+// const bigInteger = BigInt(37827327382);
+// const arr = [];
+// const obj = {};
+// const und = undefined;
+// const nu = null;
+
+// console.log(typeof bigInteger)
+// console.log(typeof str)
+// console.log(typeof num)
+// console.log(typeof arr)
+// console.log(typeof obj)
+// console.log(typeof und)
+// console.log(typeof symb)
+// console.log(typeof bool)
+
+// Приведення типів
+
+// console.log(Boolean(-1))
+// console.log(Boolean(0))
+// console.log(Boolean(''))
+// console.log(Boolean(' '))
+// console.log(Number("   777   "))
+// console.log(Number(" 777sfgah  "))
+// console.log(Number(true))
+// console.log(Number(false))
+
+// boxing
+
+// const str = 7777
+// const res = str.toString()
+// console.log(res)
+
+//TODO: Навіщо потрібен Symbol
+// Це примітивный тип даних, як boolean чи string, Який потрібен для створення унікальних ідентифікаторів.
+
+// Існують «глобальні символи», вони доступні в усіх частинах вашої програми.
+// Тобто ви можете створити символ і помістити його в якусь базу, це робиться за допомогою функції Symbol.for()
+
+// console.log(Symbol('Hello') === Symbol('Hello'))
+
+// const someName = Symbol.for('Some identifier');
+
+// console.log(Symbol.for('Some identifier') === someName)
---
+++ lesson-1/module-1-2/variables.js
@@ -0,0 +1,50 @@
+//TODO: оголошення змінних
+
+// let a = 'Pete';
+// const b = 'John';
+// var c = 'Igor';
+
+// const newArr = [...arr];
+
+// console.log(newArr, arr)
+
+// Приймає один або кілька елементів, які додадуться до масиву та повертають довжину масиву
+// const word = 'Alona'
+// const res = arr.push(word, 'dobavili');
+// console.log(res);
+
+// 3 параметра
+// 1 - індекс по якому починає змінюватися масив
+// 2 - число, що показує кількість старих елементів, що видаляються з масиву
+// 3 - Необов'язкові параметри. Елементи, що додаються до масиву. Якщо ви не вкажете жодного елемента
+// Повертає новий масив вирізаних елементів
+// const arrSplice = arr.splice(0,2, 'Marina', "array"
+// arrSplice.push('Hello')
+// console.log(arrSplice, arr)
+
+// Додає елементи на початок масиву і ПОВЕРТАЄ ДОВЖИНУ МАСИВУ
+// const length = arr.unshift('some string', 'unshift');
+// console.log(arr);
+
+// видаляє перший елемент з масиву та повертає його значення. Цей метод змінює довжину масиву
+// const shiftArr = arr.shift();
+// console.log(shiftArr, arr);
+
+// Метод pop() видаляє останній елемент з масиву та повертає його значення. Якщо масив порожній - поверне undefined
+
+// chaining
+
+// const arr = ['John', 'Igor', 'Sasha', 'Chuvak', 'Bob'];
+
+// const res = arr.splice(0, 3, 'chaining').push('chaining2')
+
+// console.log(res)
+
+// console.log(res)
+
+// console.log(res)
+// arr
+// .push('some words')
+// .shift()
+
+// console.log(arr);
