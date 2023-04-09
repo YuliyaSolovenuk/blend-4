d3f040be5aa6840c1b07a583a580e8581124f680
Bliznech <120974217+Bliznech@users.noreply.github.com>
2023-04-09 15:13:07 +0200

    task1-2

--- lesson-2/module-4/modul-4.js
+++ lesson-2/module-4/modul-4.js
@@ -6,7 +6,19 @@
 //greet(name) - коллбек, що приймає ім'я і логірує в консоль
 //Рядок "Привіт <name>"
 //Реалізуй перевірку, що prompt не порожній
+// function letMeSeeYourName(callback) {
+//   const name = prompt("enter your name");
+//   if (name) {
+//     callback(name);
+//     return;
+//   }
+//   console.log("pales enter your name");
+// }
 
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
+// letMeSeeYourName(greet);
 //TODO:=================02=============================
 //Напишіть дві функції
 //makeProduct(name, price, callback) - приймає
@@ -16,6 +28,20 @@
 //Передаючи йому створений об'єкт.
 //showProduct(product) - коллбек приймаючий об'єкт
 //продукту і логірующий їх у консоль
+// function makeProduct(name, price, callback) {
+//   const id = Date.now();
+//   const product = {
+//     id,
+//     name,
+//     price,
+//   };
+//   callback(product);
+// }
+
+// function showProduct({ id, name, price }) {
+//   console.log(`id:${id}, name:${name}, price:${price}`);
+// }
+// makeProduct("milck", 50, showProduct);
 
 //TODO:=================03=============================
 // Напишіть функцію makeShef(shefName), яка повертає функцію
@@ -57,87 +83,87 @@ const array = [3, 5, 6, 34, 8, 83, 12, 34];
 
 const users = [
   {
-    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
-    name: 'Moore Hensley',
-    email: 'moorehensley@indexia.com',
-    eyeColor: 'blue',
-    friends: ['Sharron Pace'],
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
     isActive: false,
     balance: 2811,
-    skills: ['ipsum', 'lorem'],
-    gender: 'male',
+    skills: ["ipsum", "lorem"],
+    gender: "male",
     age: 37,
   },
   {
-    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
-    name: 'Sharlene Bush',
-    email: 'sharlenebush@tubesys.com',
-    eyeColor: 'blue',
-    friends: ['Briana Decker', 'Sharron Pace'],
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
     isActive: true,
     balance: 3821,
-    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-    gender: 'female',
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
     age: 34,
   },
   {
-    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
-    name: 'Ross Vazquez',
-    email: 'rossvazquez@xinware.com',
-    eyeColor: 'green',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
     isActive: false,
     balance: 3793,
-    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-    gender: 'male',
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
     age: 24,
   },
   {
-    id: '249b6175-5c30-44c6-b154-f120923736f5',
-    name: 'Elma Head',
-    email: 'elmahead@omatom.com',
-    eyeColor: 'green',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
     isActive: true,
     balance: 2278,
-    skills: ['adipisicing', 'irure', 'velit'],
-    gender: 'female',
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
     age: 21,
   },
   {
-    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
-    name: 'Carey Barr',
-    email: 'careybarr@nurali.com',
-    eyeColor: 'blue',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
     isActive: true,
     balance: 3951,
-    skills: ['ex', 'culpa', 'nostrud'],
-    gender: 'male',
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
     age: 27,
   },
   {
-    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
-    name: 'Blackburn Dotson',
-    email: 'blackburndotson@furnigeer.com',
-    eyeColor: 'brown',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
     isActive: false,
     balance: 1498,
-    skills: ['non', 'amet', 'ipsum'],
-    gender: 'male',
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
     age: 38,
   },
   {
-    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
-    name: 'Sheree Anthony',
-    email: 'shereeanthony@kog.com',
-    eyeColor: 'brown',
-    friends: ['Goldie Gentry', 'Briana Decker'],
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
     isActive: true,
     balance: 2764,
-    skills: ['lorem', 'veniam', 'culpa'],
-    gender: 'female',
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
     age: 39,
   },
 ];
